037e2f0dbe04ca52f9a4805e0d52c69c5804dd3b
Mario Corchero-jimenez <MCORCHEROJIM@bloomberg.net>
2016-12-25 18:57:35 -0500

    Add app entrypoint script

--- setup.py
+++ setup.py
@@ -41,4 +41,9 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    entry_points={
+        'console_scripts': [
+            'dothub=dothub.__main__:main'
+        ]
+    },
 )
