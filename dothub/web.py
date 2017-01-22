fc0084ddd42196f1f51cd3c7fc0cd861831da313
Mario Corchero-jimenez <MCORCHEROJIM@bloomberg.net>
2017-01-22 19:46:11 +0000

    Improve handling of github events
    Check the file being updated and react based on it.

--- dothub/web.py
+++ dothub/web.py
@@ -2,6 +2,7 @@ import flask
 import werkzeug.exceptions
 import logging
 import os
+import itertools
 from dothub.repository import Repo
 from dothub.organization import Organization
 from dothub import github_helper
@@ -51,41 +52,47 @@ def index():
     return "TODO"
 
 
-@app.route("/github", methods=['POST'])
-def github():
-    """Main function that handles all github hook events"""
-    actions = []  # Actions to perform
-    try:
-        event = flask.request.headers['X-GitHub-Event']
-        data = flask.request.get_json() or {}
-        repo_full_name = data["repository"]["full_name"]
-        repo_owner = data["repository"]["owner"]["name"]
-        repo_name = data["repository"]["name"]
-    except KeyError:
-        LOG.info("Missing data in request, rejecting", exc_info=True)
-        raise werkzeug.exceptions.BadRequest("Missing data on request,"
-                                             " Are you a GitHub hook event?")
-
-    LOG.info("Handling {} event for {}".format(event, repo_full_name))
-
-    # This code needs quite a refactor, it is late and I just want to sleep...
-    # I know, so unprofessional...
-
-    # REPO
-    repo_changes = False
-    repo = Repo(GH_HELPER, repo_owner, repo_name)
+def repo_update(org, repo):
+    """Updates the repo configuration using the dothub file in it"""
+    repo = Repo(GH_HELPER, org, repo)
     try:
         file_repo_config = yaml.safe_load(repo.get_file(REPO_CONFIG_FILE))
     except requests.exceptions.HTTPError:
         pass
     else:
         current_repo_config = repo.describe()
-        repo_changes = check_changes(current_repo_config, file_repo_config)
+        LOG.info("Checking if any repo changes")
+        if check_changes(current_repo_config, file_repo_config):
+            LOG.info("Updating repo configuration")
+            repo.update(file_repo_config)
+            return ["repo_update"]
+    return []
 
-    # ORG
-    org_changes = False
+
+def repo_sync(org, repo):
+    """Updates the file config based on the current configuration"""
+    repo = Repo(GH_HELPER, org, repo)
     try:
-        org = Organization(GH_HELPER, repo_owner)
+        file_repo_config = yaml.safe_load(repo.get_file(REPO_CONFIG_FILE))
+    except requests.exceptions.HTTPError:
+        pass  # Don't update if there is not file already
+    else:
+        current_repo_config = repo.describe()
+        LOG.info("Checking if any repo changes")
+        if check_changes(file_repo_config, current_repo_config):
+            for key in [_ for _ in current_repo_config if _ not in file_repo_config]:
+                current_repo_config.pop(key)
+            LOG.info("Updating repo config file")
+            repo.update_file(REPO_CONFIG_FILE, utils.serialize_yaml(current_repo_config))
+            return ["repo_sync"]
+    return []
+
+
+def org_update(org, repo):
+    """Updates the oeg configuration using the dothub file in a repo"""
+    repo = Repo(GH_HELPER, org, repo)
+    try:
+        org = Organization(GH_HELPER, org)
     except requests.exceptions.HTTPError:
         pass
     else:
@@ -96,35 +103,79 @@ def github():
         else:
             current_org_config = org.describe()
             LOG.info("Checking if any org change...")
-            org_changes = check_changes(current_org_config, file_org_config)
+            if check_changes(current_org_config, file_org_config):
+                LOG.info("Updating org configuration")
+                org.update(file_org_config)
+                return ["org_update"]
+    return []
+
+
+def org_sync(org, repo):
+    """Updates the file config for the org based on the current configuration"""
+    repo = Repo(GH_HELPER, org, repo)
+    try:
+        org = Organization(GH_HELPER, org)
+    except requests.exceptions.HTTPError:
+        pass  # Don't update if there is not file already
+    else:
+        try:
+            file_org_config = yaml.safe_load(repo.get_file(ORG_CONFIG_FILE))
+        except requests.exceptions.HTTPError:
+            pass
+        else:
+            current_org_config = org.describe()
+            LOG.info("Checking if any org change...")
+            for key in [_ for _ in current_org_config if _ not in file_org_config]:
+                current_org_config.pop(key)
+            if check_changes(file_org_config, current_org_config):
+                LOG.info("Updating repo config file")
+                repo.update_file(REPO_CONFIG_FILE, utils.serialize_yaml(current_org_config))
+                return ["org_sync"]
+    return []
+
+
+@app.route("/github", methods=['POST'])
+def github():
+    """Main function that handles all github hook events"""
+    actions = []  # Actions to perform
+    try:
+        event = flask.request.headers['X-GitHub-Event']
+        data = flask.request.get_json() or {}
+        repo_full_name = data["repository"]["full_name"]
+    except KeyError:
+        LOG.info("Missing data in request, rejecting", exc_info=True)
+        raise werkzeug.exceptions.BadRequest("Missing data on request,"
+                                             " Are you a GitHub hook event?")
+
+    LOG.info("Handling {} event for {}".format(event, repo_full_name))
 
     if event == "ping":
         return "pong"
-    elif event == "push":
-        if repo_changes:
-            repo.update(file_repo_config)
-            actions.append("repo_update")
 
-        if org_changes:
-            org.update(file_org_config)
-            actions.append("org_update")
+    elif event == "push":
+        owner = data["repository"]["owner"]
+        repo_owner = owner.get("name", owner.get("login"))
+        assert repo_owner
+        repo_name = data["repository"]["name"]
+        updated_files = list(itertools.chain(*[c["modified"] for c in data["commits"]]))
+        LOG.info(updated_files)
+
+        if REPO_CONFIG_FILE in updated_files:
+            actions += repo_update(repo_owner, repo_name)
+        else:
+            actions += repo_sync(repo_owner, repo_name)
+
+        if ORG_CONFIG_FILE in updated_files:
+            actions += org_update(repo_owner, repo_name)
+        else:
+            actions += org_sync(repo_owner, repo_name)
 
     else:  # any other hook
-        if repo_changes:
-            for key in [_ for _ in current_repo_config if _ not in file_repo_config]:
-                current_repo_config.pop(key)
-            LOG.info("Updating repo config file")
-            repo.update_file(REPO_CONFIG_FILE, utils.serialize_yaml(current_repo_config))
-            actions.append("repo_sync")
-
-        if org_changes:
-            for key in [_ for _ in current_org_config if _ not in file_org_config]:
-                current_org_config.pop(key)
-            org.update(current_org_config)
-            LOG.info("Updating org config file")
-            repo.update_file(ORG_CONFIG_FILE, utils.serialize_yaml(current_org_config))
-            actions.append("org_sync")
-
+        if "repository" in data:
+            repo_owner = data["repository"]["owner"]["name"]
+            repo_name = data["repository"]["name"]
+            actions += repo_sync(repo_owner, repo_name)
+            actions += org_sync(repo_owner, repo_name)
 
     return flask.jsonify(
         success=True,
